6ace0a687e20bee5d53a23fdbd5ed0fd506b75ee
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2022-05-13 13:45:04 -0700

    Time: 136 ms (81.11%), Space: 46.4 MB (94.72%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function(s) {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+  let result = 0;
+  let prev = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    const current = map[s[i]];
+    if (current < prev) {
+      result -= current;
+    } else {
+      result += current;
+    }
+    prev = current;
+  }
+  return result;
+}
