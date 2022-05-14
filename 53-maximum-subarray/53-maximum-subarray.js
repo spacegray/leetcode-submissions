194d55f8e71d11d33d943be2ad4ae0c89f594ae8
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2022-05-14 13:40:16 -0700

    Time: 111 ms (45.96%), Space: 49.9 MB (93.36%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    let curr = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        curr = Math.max(nums[i], curr + nums[i]);
+        max = Math.max(max, curr);
+    }
+    return max;
+}
