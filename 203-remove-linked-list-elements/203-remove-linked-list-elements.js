d65535e4609c850108b55fcb1f893932e1772b6c
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2022-05-17 13:39:56 -0700

    Time: 102 ms (53.07%), Space: 46.6 MB (69.51%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if (!head) return null;
+    let temp = new ListNode(0);
+    temp.next = head;
+    let current = temp;
+    while (current.next) {
+        if (current.next.val === val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return temp.next;
+    
+};
