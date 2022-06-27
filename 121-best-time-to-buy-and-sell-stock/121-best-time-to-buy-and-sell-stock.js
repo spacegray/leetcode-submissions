1702aee41c9d6a76fe3c88828221ada6d555bda3
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2022-06-26 23:04:51 -0700

    Time: 84 ms (95.96%), Space: 52.1 MB (10.76%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+
+var maxProfit = function(prices) {
+    let min = prices[0];
+    let max = 0;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i];
+            
+        } else if (prices[i] - min > max) {
+            max = prices[i] - min;
+        }
+    }
+    return max;
+}
