955d054f14042e49a3d8e3f0c65815b6baa1fac4
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2023-01-26 12:48:29 -0800

    Time: 102 ms (12.29%), Space: 42.4 MB (29.62%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const result = [];
+    for (let i = 0; i < numRows; i++) {
+        const row = [];
+        for (let j = 0; j <= i; j++) {
+            if (j === 0 || j === i) {
+                row.push(1);
+            } else {
+                row.push(result[i - 1][j - 1] + result[i - 1][j]);
+            }
+        }
+        result.push(row);
+    }
+    return result;
+    
+};
