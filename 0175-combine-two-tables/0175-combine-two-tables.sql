73fab97397e58b383136b6e20e8d1a84da2951af
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2023-02-07 11:38:30 -0800

    Time: 1114 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -5,3 +5,10 @@ FROM Person
 LEFT JOIN Address
 ON Person.personId = Address.personId
 
+/*
+My reasoning:
+1. I used a LEFT JOIN because I wanted to include all the rows from the Person table, 
+    even if they didn't have a corresponding row in the Address table.
+2. I used ON Person.personId = Address.personId to join the two tables on the personId column.
+3. I used SELECT firstName, lastName, city, state to select the columns I wanted to return.
+*/
