578b661309451b258dd0e858e559362243046410
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2023-04-10 09:29:38 -0700

    Time: 57 ms (57.89%), Space: 42.9 MB (57.89%) - LeetHub

---
+++ 0293-flip-game/0293-flip-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} currentState
+ * @return {string[]}
+ */
+var generatePossibleNextMoves = function(currentState) {
+    let result = [];
+    for (let i = 0; i < currentState.length - 1; i++) {
+        if (currentState[i] === '+' && currentState[i + 1] === '+') {
+            result.push(currentState.slice(0, i) + '--' + currentState.slice(i + 2));
+        }
+    }
+    return result;
+    
+};
