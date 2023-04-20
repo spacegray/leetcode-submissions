bd94a0b014fe5d9e4858fcb0b7ca3ab01c06623f
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2023-04-20 12:35:44 -0700

    Time: 75 ms (72.25%), Space: 48.6 MB (64.14%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,28 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (!root) return null;
+    let queue = [root];
+    while (queue.length) {
+        let n = queue.length;
+        for (let i = 0; i < n; i++) {
+            let node = queue.shift();
+            if (i < n - 1) node.next = queue[0];
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+    return root;
+};
