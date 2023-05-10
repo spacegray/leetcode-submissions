0d1da6625375daca9948c3a1a352f69edc56ab73
Ayla Basha <68717338+spacegray@users.noreply.github.com>
2023-05-10 15:48:23 -0700

    Time: 76 ms (8.33%), Space: 43.9 MB (85.71%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.ts
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.ts
@@ -1,40 +1,33 @@
 function generateMatrix(n: number): number[][] {
-  const matrix: number[][] = [];
-  for (let i = 0; i < n; i++) {
-    matrix.push(new Array(n).fill(0));
-  }
-
+  const result: number[][] = Array(n).fill(0).map(() => Array(n).fill(0));
   let num = 1;
-  let left = 0, right = n - 1, top = 0, bottom = n - 1;
-
-  while (left <= right && top <= bottom) {
-    // Traverse right
-    for (let i = left; i <= right; i++) {
-      matrix[top][i] = num++;
+  let rowStart = 0, rowEnd = n - 1, colStart = 0, colEnd = n - 1;
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    for (let col = colStart; col <= colEnd; col++) {
+      result[rowStart][col] = num++;
     }
-
-    // Traverse down
-    for (let i = top + 1; i <= bottom; i++) {
-      matrix[i][right] = num++;
+    for (let row = rowStart + 1; row <= rowEnd; row++) {
+      result[row][colEnd] = num++;
     }
-
-    if (left < right && top < bottom) {
-      // Traverse left
-      for (let i = right - 1; i >= left; i--) {
-        matrix[bottom][i] = num++;
+    if (rowStart < rowEnd && colStart < colEnd) {
+      for (let col = colEnd - 1; col > colStart; col--) {
+        result[rowEnd][col] = num++;
       }
-
-      // Traverse up
-      for (let i = bottom - 1; i > top; i--) {
-        matrix[i][left] = num++;
+      for (let row = rowEnd; row > rowStart; row--) {
+        result[row][colStart] = num++;
       }
     }
-
-    left++;
-    right--;
-    top++;
-    bottom--;
+    rowStart++;
+    rowEnd--;
+    colStart++;
+    colEnd--;
   }
-
-  return matrix;
+  return result;
 }
+
+/* 
+This strategy works by keeping track of four variables, rowStart, rowEnd, colStart, and colEnd, that represent the boundaries of the current spiral. It then uses a single loop to iterate over the elements of the spiral, starting with the top row and moving clockwise around the perimeter. The loop terminates when the boundaries of the spiral have crossed over each other.
+
+In each iteration of the loop, we first fill in the top row, then the right column, then the bottom row (if applicable), and finally the left column (if applicable). We also increment the value of num each time we fill in a cell.
+
+*/
